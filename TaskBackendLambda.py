3bf0c7c823380ef4b757878737808a8645ce0ed5
yadav13Uwindsor <99781736+yadav13Uwindsor@users.noreply.github.com>
2022-10-24 18:07:05 -0400

    Update TaskBackendLambda.py

--- TaskBackendLambda.py
+++ TaskBackendLambda.py
@@ -4,8 +4,8 @@ import boto3
 
 rds_client = boto3.client('rds-data')
 database_name = 'ipdatabase'
-db_cluster_arn = 'arn:aws:rds:us-east-2:026802451096:cluster:internshipprojectcluster'
-db_credentials_secrets_store_arn = 'arn:aws:secretsmanager:us-east-2:026802451096:secret:rds-db-credentials/cluster-UFR65HZXVBFQXE2RXVEUBI3UP4/admin/1666216261131-tOj4Oz'
+db_cluster_arn = 'arn:aws:rds:us-east-2:026802451096:cluster:ipdatabase-cluster'
+db_credentials_secrets_store_arn = 'arn:aws:secretsmanager:us-east-2:026802451096:secret:rds-db-credentials/cluster-3V4ZRVTP2PSUNQKECQIA4RGFVY/admin/1666484218072-hdxza0'
 
 
 def lambda_handler(event, context):
@@ -15,21 +15,43 @@ def lambda_handler(event, context):
     httpmethod=event['httpMethod']
     user_id=event['queryStringParameters']['user_id']
     
-    if path=='/getalltasks' :
-        response = execute_statement('SELECT * FROM ipdatabase.tasks where user_id='+str(user_id))
+    if path=='/getalltasks':
+        querystring = 'SELECT * FROM ipdatabase.tasks where user_id= %d '%(int(user_id))
     
-    if path=='/tasks' and httpmethod=='POST':
+    if path=='/newtask':
         task_title=event['queryStringParameters']['title']
         task_description=event['queryStringParameters']['description']
         querystring = 'insert into tasks (user_id,task_title,task_description,task_status,task_date) values ("%s","%s","%s","active",now())'%(user_id,task_title,task_description)
-        response = execute_statement(querystring)
         
-    if path=='/tasks/update' and httpmethod=='PATCH':
+    if path=='/updatetask':
+        task_id=event['queryStringParameters']['id']
         task_title=event['queryStringParameters']['title']
         task_description=event['queryStringParameters']['description']
-        querystring = 'insert into tasks (user_id,task_title,task_description,task_status,task_date) values ("%s","%s","%s","active",now())'%(user_id,task_title,task_description)
+        
+        querystring = 'UPDATE tasks SET task_title = "%s", task_description= "%s", task_date=now() WHERE task_id = %d;'%(task_title,task_description,int(task_id))
+        
+    if path=='/completetask':
+        task_id=event['queryStringParameters']['id']
+        querystring = 'UPDATE tasks SET task_status = "completed" WHERE task_id = %d'%(int(task_id))
+        
+    
+    if path=='/removetask':
+        task_id=event['queryStringParameters']['id']
+        querystring = 'DELETE FROM tasks WHERE task_id = %d'%(int(task_id))
+    
+    
+    response = execute_statement(querystring)
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': str(response)
+    }
 
-    return response;
     
 def execute_statement(sql):
     response = rds_client.execute_statement(
@@ -38,12 +60,9 @@ def execute_statement(sql):
         resourceArn=db_cluster_arn,
         sql=sql
     )
-    return response;
-            
-            
+    
+    return response
+    
             
     
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda'
-    }
+    
